7fe67517b4d6f49949fe02fdfe3b14b49137fd86
Kundanakonduru <kondurukundana1@gmail.com>
2026-06-19 13:58:50 +0530

    Add files via upload

---
+++ Patterns/star.py
@@ -0,0 +1,17 @@
+for i in range(0,3):
+    for j in range(0,3):
+        print("*",end=" ")
+    print()
+
+
+#using while
+i=0
+while i<=2:
+    j=0
+    while j<=2:
+        print("*",end=" ")
+        j+=1
+    print()
+    i+=1
+
+    
